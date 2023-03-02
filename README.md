f26d612ba4410310ca63a1d13edf9297db4cb896
Freewind <nowindlee@gmail.com>
2023-03-02 22:42:48 +0800

    typescript-extjs-custom-events-demo

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-TypeScript ExtJS Custom Controller ViewModel Demo
-=================================================
+TypeScript ExtJS Custom Events Demo
+===================================
 
-View中的listeners中的events对应的handler，需要注意：
-1. 如果是View中自己的listeners，会找View上定义的method
-2. 如果是View中嵌入的View的listeners，会找View中controller中定义的method，再找View上定义的method
+在ExtJs中可以定义任意名字的event，它们可能跟浏览器内置的相同，也可以完全不同。
+
+因为对于非浏览器event，需要靠代码调用来触发，实际上和调用一个方法差不多。
+
+在代码里，可以通过 `listeners: {...}`来匹配，也可以通过`component.fireEvent('custom-event', component)`来触发。
+
+比如本例中，可以通过`hello.app.mainView.fireEvent('myEvent', hello.app.mainView)`触发。
 
 ```
 npm install
--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,30 @@
 Ext.onReady(() => {
+    Ext.define('MyView', {
+        extend: 'Ext.panel.Panel',
+        renderTo: 'main',
+        height: 100,
+        width: 200,
+        title: 'Hello world',
+        controller: new Ext.app.ViewController({
+            onMyEvent: () => {
+                alert('### controller.onMyEvent');
+            }
+        }),
+        listeners: {
+            myEvent: 'onMyEvent'
+        },
+        items: [
+            new Ext.button.Button({
+                text: 'button',
+                listeners: {
+                    click: 'onMyEvent'
+                }
+            })
+        ]
+    })
     new Ext.app.Application({
         name: 'hello',
-        mainView: new Ext.panel.Panel({
-            renderTo: 'main',
-            height: 100,
-            width: 200,
-            title: 'Hello world',
-            bind: {
-                html: 'Hello, {username}'
-            },
-            viewModel: new Ext.app.ViewModel({
-                data: {
-                    username: 'username from viewModel'
-                }
-            }),
-            controller: new Ext.app.ViewController({
-                onClickUsername: () => {
-                    alert('### controller.onClickUsername');
-                }
-            }),
-            fnOfTheView: () => {
-                alert('### fnOfTheView')
-            },
-            listeners: {
-                click: {
-                    element: 'el',
-                    // NOTE view中的fn指向的是view自己定义的method
-                    fn: 'fnOfTheView'
-                }
-            },
-            items: [
-                new Ext.button.Button({
-                    text: 'button',
-                    listeners: {
-                        // NOTE 嵌入的'fn'会先指向controller中的method，然后是view自己的method
-                        click: 'onClickUsername',
-                    }
-                })
-            ]
-        }),
+        mainView: Ext.create('MyView'),
         launch: () => {
             console.log("### launch")
             return true;
